da86ef810bf02ebe745775eb78205a5755b2ffc2
Qkston <qkston22@gmail.com>
2021-04-06 16:24:19 +0300

    Add full course work

---
+++ OOP/2 semestr/Курсова робота/Програма/Курсова/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Курсова
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.equation = new System.Windows.Forms.TextBox();
+            this.promX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.minimum = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.example1 = new System.Windows.Forms.Button();
+            this.example2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(993, 619);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 51);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Побудувати графік";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Construction_graph_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1182, 619);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 51);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Deleted_Click);
+            // 
+            // graph
+            // 
+            this.graph.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.graph.BackColor = System.Drawing.Color.Transparent;
+            chartArea4.Name = "ChartArea1";
+            this.graph.ChartAreas.Add(chartArea4);
+            this.graph.Location = new System.Drawing.Point(-2, 0);
+            this.graph.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.graph.Name = "graph";
+            series4.BorderColor = System.Drawing.Color.White;
+            series4.BorderWidth = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Color = System.Drawing.Color.Red;
+            series4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series4.IsXValueIndexed = true;
+            series4.LabelBorderWidth = 0;
+            series4.LegendText = "Графік";
+            series4.Name = "Series1";
+            series4.ShadowColor = System.Drawing.Color.Silver;
+            series4.YValuesPerPoint = 2;
+            this.graph.Series.Add(series4);
+            this.graph.Size = new System.Drawing.Size(1256, 613);
+            this.graph.TabIndex = 5;
+            this.graph.Text = "chart1";
+            // 
+            // equation
+            // 
+            this.equation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.equation.Location = new System.Drawing.Point(57, 636);
+            this.equation.Name = "equation";
+            this.equation.Size = new System.Drawing.Size(114, 20);
+            this.equation.TabIndex = 1;
+            // 
+            // promX
+            // 
+            this.promX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.promX.Location = new System.Drawing.Point(200, 636);
+            this.promX.Name = "promX";
+            this.promX.Size = new System.Drawing.Size(103, 20);
+            this.promX.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 637);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "y(x) =";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 619);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Введіть проміжок Х";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 619);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Введіть вашу функцію";
+            // 
+            // minimum
+            // 
+            this.minimum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.minimum.Location = new System.Drawing.Point(334, 636);
+            this.minimum.Name = "minimum";
+            this.minimum.Size = new System.Drawing.Size(103, 20);
+            this.minimum.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(331, 619);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Точність мінімуму";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1141, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 18;
+            // 
+            // example1
+            // 
+            this.example1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.example1.Location = new System.Drawing.Point(720, 627);
+            this.example1.Name = "example1";
+            this.example1.Size = new System.Drawing.Size(130, 37);
+            this.example1.TabIndex = 19;
+            this.example1.Text = "Приклад 1";
+            this.example1.UseVisualStyleBackColor = true;
+            this.example1.Click += new System.EventHandler(this.Example1_Click);
+            // 
+            // example2
+            // 
+            this.example2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.example2.Location = new System.Drawing.Point(856, 627);
+            this.example2.Name = "example2";
+            this.example2.Size = new System.Drawing.Size(117, 37);
+            this.example2.TabIndex = 20;
+            this.example2.Text = "Приклад 2";
+            this.example2.UseVisualStyleBackColor = true;
+            this.example2.Click += new System.EventHandler(this.Example2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1254, 681);
+            this.Controls.Add(this.example2);
+            this.Controls.Add(this.example1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.minimum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.promX);
+            this.Controls.Add(this.equation);
+            this.Controls.Add(this.graph);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(1270, 720);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Graphics Builder";
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart graph;
+        private System.Windows.Forms.TextBox equation;
+        private System.Windows.Forms.TextBox promX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox minimum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button example1;
+        private System.Windows.Forms.Button example2;
+    }
+}
+
---
+++ OOP/2 semestr/Курсова робота/Програма/Курсова/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Drawing.Drawing2D;
+
+namespace Курсова
+{
+    public partial class Form1 : Form
+    {
+        public int Y_axis;
+        public int X_axis;
+
+        public double hz;
+
+        public double first_num;
+        public double second_num;
+
+        public int num_c;
+        public int num_s;
+
+        double x0, x1, x2, x3;
+        double fx0, fx1, fx2, fx3;
+
+        Graphics g;
+
+        public double[,] array = new double[4, 25];
+        public double[,] func;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Construction_graph_Click(object sender, EventArgs e)
+        {
+            Draw_G();
+        }
+        public void Draw_G() //Відмальовка графіка та будування функції і точок на ньому
+        {
+            g = graph.CreateGraphics();
+
+            string num = promX.Text;
+            string[] str = num.Split(' ');
+
+            first_num = double.Parse(str[0]) - 1; //Перше число проміжку (початок)
+            second_num = double.Parse(str[1]) + 1; //Друге число проміжку (кінець)
+            
+            Y_axis = 1;
+
+            if (first_num >= 0 && second_num > 0)
+            {
+                num_c = Convert.ToInt32(second_num) - Convert.ToInt32(first_num);
+                num_s = graph.Height / num_c;
+            }
+            else if (first_num < 0 && second_num < 0)   
+            {
+                num_c = Math.Abs(Convert.ToInt32(first_num) - Convert.ToInt32(second_num));
+                num_s = graph.Height / num_c;
+                Y_axis = num_s * num_c; //Задаємо значення осі Y
+            }
+            else
+            {
+                num_c = Convert.ToInt32(second_num) + Math.Abs(Convert.ToInt32(first_num));
+                num_s = graph.Height / num_c;
+                Y_axis = num_s * Convert.ToInt32(Math.Abs(first_num)); //Задаємо значення осі Y
+            }
+
+            X_axis = num_s * (num_c / 2); //Задаємо значення осі X
+
+            hz = graph.Height / num_c;
+
+            func = GetAllPoints();
+            BuildFunc();
+            MinValue_Func(first_num + 1, second_num - 1);
+        }
+        public void BuildFunc() //Відмальовка функції
+        {
+            Pen for_func = new Pen(Brushes.Blue, 2);
+
+            Axis();
+
+            for (int i = 0; i < graph.Width - 1; i++)
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias; //Задаємо якість відмальовки функції
+
+                Point num1 = new Point(i, Convert.ToInt32((graph.Height / num_c) * (num_c / 2) - ((graph.Height / num_c) * func[1, i]))); //Перша частина графіку
+                
+                Point num2 = new Point(i + 1, Convert.ToInt32((graph.Height / num_c) * (num_c / 2) - (graph.Height / num_c * func[1, i + 1]))); //Друга частина графіку
+
+                g.DrawLine(for_func, num1, num2);
+            }
+        }
+        public void Axis()
+        {
+            Pen for_axis = new Pen(Brushes.Black, 2); //для осей
+
+            g.DrawLine(for_axis, new Point(num_s * Convert.ToInt32(Math.Abs(first_num)), 0), new Point(num_s * Convert.ToInt32(Math.Abs(first_num)), graph.Height)); //Малюємо вісь Y
+
+            g.DrawLine(for_axis, new Point(0, num_s * (num_c / 2)), new Point(graph.Width, num_s * (num_c / 2))); //Малюємо вісь Х
+        }
+        public double[,] GetAllPoints() //Знаходження точок для функції
+        {
+            double[,] array = new double[2, graph.Width];
+
+            double output;
+
+            double x = first_num;
+
+            for (int i = 0; i < graph.Width; i++)
+            {
+                string ins = equation.Text.Replace("x", x.ToString("F", CultureInfo.CreateSpecificCulture("en-US"))); //Перетворюємо значення функції на CultureInfo, що представляє конкретну культуру, пов'язану із вказаною назвою
+                output = Convert.ToDouble(new DataTable().Compute(ins, "")); //Використаємо метод DataTable().Compute для виконання операцій над даними
+
+                array[0, i] = x;
+                array[1, i] = output;
+                x = x + 1 / Convert.ToDouble(num_s);
+
+            }
+
+            return array;
+        }
+        public void MinValue_Func(double first_num, double second_num)
+        {
+            x0 = first_num;
+            x1 = (first_num + second_num) / 2;
+            x2 = second_num;
+            x3 = 0;
+            
+            fx0 = C_Func(x0);
+            fx1 = C_Func(x1);
+            fx2 = C_Func(x2);
+            fx3 = 0;
+
+            double min = Convert.ToDouble(minimum.Text);
+
+            double xx = (x0 + x1) / 2 - (fx1 - fx0) * (x2 - x0) * (x2 - x1) / (2 * ((fx1 - fx0) * (x2 - x0) - (fx2 - fx0) * (x1 - x0)));
+            double fxx = C_Func(xx - 1);
+
+            array[0, 0] = x0;
+            array[1, 0] = x1;
+            array[2, 0] = x2;
+            array[3, 0] = xx;
+            
+            while (x1 - xx >= min && x2 - xx >= min)
+            {
+                if (xx < x1)
+                {
+                    x3 = x2; fx3 = fx2; x2 = x1; fx2 = fx1; x1 = xx; fx1 = fxx;
+                }
+                else if(xx > x1)
+                {
+                    x3 = x2; fx3 = fx2; fx2 = xx; fx2 = fxx;
+                }
+                if (fx1 > fx2)
+                {
+                    x0 = x1; fx0 = fx1; x1 = x2; fx1 = fx2; x2 = x3; fx2 = fx3;
+                }
+            }
+
+            PrintAllSteps();
+            Draw_P();
+        }
+        public double C_Func(double x) //Обчислення точок
+        {
+            string str = equation.Text.Replace("x", x.ToString("F", CultureInfo.CreateSpecificCulture("en-US"))); //Перетворюємо значення функції на CultureInfo, що представляє конкретну культуру, пов'язану із вказаною назвою
+            double fx = Convert.ToDouble(new DataTable().Compute(str, "")); //Використаємо метод DataTable().Compute для виконання операцій над даними
+            return fx;
+        }
+        public void PrintAllSteps() //Друкування точок які були знайдені методом парабол
+        {
+            label5.Text = "x0 = " + Math.Round(array[0, 0], 4) + "\nx1 = " + Math.Round(array[1, 0], 4) + "\nx2 = " + Math.Round(array[2, 0], 4) + "\nx* = " + Math.Round(array[3, 0], 4); //Виписуємо значення точок
+        }
+        public void Draw_P() //Відмальовка точок які були знайдені методом парабол
+        {
+            SolidBrush color = new SolidBrush(Color.Black); //Для точок
+
+            string[] text = new string[4] { "x0", "x1", "x2", "x*" }; //Задаємо букви для графіка
+
+            for (int i = 0; i < array.GetLength(0); i++) //Малюємо точки та букви на графіку
+            {
+                RectangleF Drawpoint = new RectangleF(Convert.ToInt32(Y_axis + array[i, 0] * hz), Convert.ToInt32(X_axis - C_Func(array[i, 0]) * hz) - 5, 8, 8); //Задаємо координати точок для функції
+                g.FillEllipse(color, Drawpoint); //Малюємо точки для функції
+
+                RectangleF x_axis = new RectangleF(Convert.ToInt32(Y_axis + array[i, 0] * hz) - 3, X_axis - 5, 8, 8); //Задаємо координати для точок для осі Х
+                RectangleF x_axis2 = new RectangleF(Convert.ToInt32(Y_axis + array[i, 0] * hz) - 3, X_axis + 4, 20, 15); //Задаємо координати для букв на осі Х
+                g.FillEllipse(color, x_axis); //Малюємо точки на осі X
+                g.DrawString(text[i], label1.Font, color, x_axis2); //Малюємо букви на осі Х
+
+                RectangleF y_axis = new RectangleF(Y_axis - 5, Convert.ToInt32(X_axis - C_Func(array[i, 0]) * hz) - 5, 8, 8); //Задаємо координати для точок для осі Y
+                RectangleF y_axis2 = new RectangleF(Y_axis + 5, Convert.ToInt32(X_axis - C_Func(array[i, 0]) * hz) + 3, 30, 15); //Задаємо координати для букв на осі Y
+                g.FillEllipse(color, y_axis); //Малюємо точки на осі Y
+                string str = "f(" + text[i] + ")";
+                g.DrawString(str, label1.Font, color, y_axis2); //Малюємо букви на осі Y
+            }
+        }
+        public void Deleted_Click(object sender, EventArgs e)
+        {
+            graph.Series[0].Points.Clear(); //Очищюємо графік
+            equation.Text = ""; //Очищюємо поле для вводу функції
+            promX.Text = ""; //Очищюємо поле для вводу проміжку
+            minimum.Text = ""; //Очищаємо мінімальне значення
+            label5.Text = ""; //Очищаємо значення
+        }
+        public void Example1_Click(object sender, EventArgs e)
+        {
+            equation.Text = "x*x-5";
+            promX.Text = "-2 5";
+            minimum.Text = "0,01";
+        }
+        public void Example2_Click(object sender, EventArgs e)
+        {
+            equation.Text = "x*x*x*x-2*x*x+3*x+1";
+            promX.Text = "-2 3";
+            minimum.Text = "0,5";
+        }
+    }
+}
