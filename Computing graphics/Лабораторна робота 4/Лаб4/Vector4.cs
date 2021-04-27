9f08327f9cbc59edc5529850bbfeb7b2b4501721
Qkston <qkston22@gmail.com>
2021-04-27 18:07:07 +0300

    Completed fourth laboratory work of computing graphics
    1. Побудувати матриці повороту відносно осей Oy та Ox для однорідних координат простору.
    
    2. Написати процедуру множення матриць виду на мові програмування
    
    3. Задати на екрані положенням вершин правильну трикутну піраміду.
    
    4. Написати програму, що здійснює поворот куба відносно двох осей одночасно.

---
+++ Computing graphics/Лабораторна робота 4/Лаб4/Vector4.cs
@@ -0,0 +1,16 @@
+﻿namespace Лабораторна_робота_4
+{
+    public class Vector4
+    {
+        public float X, Y, Z, W;
+
+        public Vector4(float x, float y, float z, float w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+
+    }
+}
